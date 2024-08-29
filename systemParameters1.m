3d40253d5f3b0644580d0209cdc48c9161b52742
S-SARTHAK1999 <violinistss.muse@gmail.com>
2024-08-29 16:45:28 +0530

    MATLAB Simulink files

---
+++ systemParameters1.m
@@ -0,0 +1,16 @@
+%% system parameters for spring mass damper system
+
+% Under damped system
+%m = 100;
+%c = 500;
+%k = 10000;
+
+% Over damped system
+%m = 100;
+%c = 500;
+%k = 10000;
+
+% Crtically damped system
+m = 100;
+c = 2000;
+k = 10000;
